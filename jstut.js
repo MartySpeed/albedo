3f63be0316a5c1b7c293223668720b907a4070d3
John Anthony <63317831+MartySpeed@users.noreply.github.com>
2020-06-10 08:49:27 -0700

    update
    creating portfolio for Albdeo portfolio

---
+++ jstut.js
@@ -0,0 +1,20 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number and object
+*/
+
+var myName = "John"; //using var to declare a variable to any of the data types, var can be used throughout the entire program
+
+let ourName = "martyspeed" //let a variable that we use in js for, let will only be used within the scope, where you declare that
+
+const pi = 3.14 //const is a variable that we use in js for a variable that will never change.
+
+/*STORING VALUES WITH ASSIGNMENT OPERATOR*/
+var a; //declaring a variable
+
+var b = 2; //decalring variable b and assigning it the variable 2
+
+a = 7 // assigned 7 to variable a
+
+b = a; // assigned a to b
+
+console.log(a) // allows you to see things in the console.
